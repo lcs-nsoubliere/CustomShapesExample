1aa7e1fe7adc2ed0c96d110e3be47b0c5dc6cb20
Noah Soubliere <nsoubliere@lcs.on.ca>
2022-11-17 11:41:32 -0500

    Initial Commit

---
+++ CustomShapesExample/CustomShapesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesExampleApp.swift
+//  CustomShapesExample
+//
+//  Created by Noah Alexandre Soubliere on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
